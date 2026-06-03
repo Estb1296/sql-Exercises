d9bc6d4302bdfbd814044d0dc6b66908533652a7
ezrateshale <ezra.teshale12@gmail.com>
2026-06-03 10:55:32 -0500

    Feat: Join Queries sql exercise Complete

--- joins.sql
+++ joins.sql
@@ -1,43 +1,43 @@
-SELECT ProductId, ProductName, UnitPrice,CategoryName
+SELECT ProductId, ProductName, UnitPrice, CategoryName
 From Products
 JOIN Categories
-		ON Categories.CategoryID = Products.CategoryID
+		ON  Products.CategoryID = Categories.CategoryID 
         ORDER BY CategoryName,ProductName;
         
-SELECT ProductId, ProductName, UnitPrice,CompanyName
+SELECT ProductId, ProductName, UnitPrice, CompanyName
 FROM Products
 JOIN Suppliers
-		ON Suppliers.SupplierID = Products.SupplierID
-		WHERE UnitPrice>75
+		ON Products.SupplierID = Suppliers.SupplierID 
+		WHERE UnitPrice > 75
         ORDER BY ProductName;
         
-SELECT ProductID, ProductName, UnitPrice,CategoryName,CompanyName
+SELECT ProductID, ProductName, UnitPrice,CategoryName, CompanyName
 FROM Products
 JOIN Categories
-		ON Categories.CategoryID = Products.CategoryID
+		ON  Products.CategoryID = Categories.CategoryID 
 JOIN Suppliers
-		ON Suppliers.SupplierID = Products.SupplierID
+		ON  Products.SupplierID = Suppliers.SupplierID 
         ORDER BY ProductName;
 
-SELECT ProductName,CategoryName
+SELECT ProductName, CategoryName
 FROM Products
 JOIN Categories
-		ON Categories.CategoryID = Products.CategoryID
+		ON Products.CategoryID = Categories.CategoryID 
 		WHERE UnitPrice = (SELECT max(UnitPrice) FROM northwind.Products);
         
-SELECT OrderID, ShipName, ShipAddress,CompanyName
+SELECT OrderID, ShipName, ShipAddress, CompanyName
 FROM Orders
 JOIN Shippers
 		ON Orders.ShipVia = Shippers.ShipperID
         WHERE ShipCountry = 'Germany';
         
 SELECT Orders.OrderID, Orders.OrderDate, ShipName, ShipAddress
-FROM `Order Details` as OrderDetails
+FROM `Order Details` AS OrderDetails
 JOIN Orders
 		ON OrderDetails.OrderID = Orders.OrderID
 JOIN Products
 		ON OrderDetails.ProductID = Products.ProductID
         WHERE ProductName = 'Sasquatch Ale';
-		
+
         
 	
